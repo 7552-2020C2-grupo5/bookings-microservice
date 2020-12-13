adff8900fe127c0e1300a06377958042b79a744e
CrossNox <ijmermet@gmail.com>
2020-12-13 13:10:33 -0300

    add booking date

--- booking_microservice/models.py
+++ booking_microservice/models.py
@@ -1,5 +1,6 @@
 """SQLAlchemy models."""
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -13,3 +14,4 @@ class Booking(db.Model):  # type: ignore
     total_price = db.Column(db.Float, nullable=False)
     initial_date = db.Column(db.Date, nullable=False)
     final_date = db.Column(db.Date, nullable=False)
+    booking_date = db.Column(db.DateTime, nullable=False, default=func.now())
