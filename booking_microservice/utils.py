022e3f81f8cf7259204302ea6b77541ac7592bfa
CrossNox <ijmermet@gmail.com>
2021-02-20 23:36:05 -0300

    ref

--- booking_microservice/utils.py
+++ booking_microservice/utils.py
@@ -12,11 +12,27 @@ class FilterParam:
         schema="int",
         transform=None,
         format_=None,
+        attribute=None,
         default=None,
     ):
+        """Filter by operation.
+
+        Parameters
+        ----------
+        name: Name for the filter.
+        op: Operation to filter with.
+        _in: Where the parameter is located.
+        schema: The type for swagger documentation.
+        transform: Pre-transform val.
+        format_: Format for swagger.
+        attribute: If none, uses name. The attribute to filter on.
+        default: Default value.
+        """
+
         self.name = name
         self.op = op
         self.val = None
+        self.attribute = attribute or self.name
         self.__schema__ = {"name": name, "in": _in, "type": schema, "format": format_}
         self.transform = transform
         self.default = default
@@ -26,12 +42,20 @@ class FilterParam:
 
     def __call__(self, val=None):
         if not self.default and not val:
-            raise ValueError("Should provide either val or default value")
+            raise ValueError("Should provide val or default value")
         self.val = (self.transform or self.identity)(val or self.default)
         return self
 
     def apply(self, query, model):
-        return query.filter(self.op(getattr(model, self.name), self.val))
+        if "." in self.attribute:
+            child_class, child_attr = self.attribute.split(".")
+            child_class_ = model.__mapper__.relationships[child_class].mapper.class_
+            return query.filter(
+                getattr(model, child_class).any(
+                    self.op(getattr(child_class_, child_attr), self.val)
+                )
+            )
+        return query.filter(self.op(getattr(model, self.attribute), self.val))
 
     def __repr__(self):
         return f"filter {self.name} by {self.op}"
