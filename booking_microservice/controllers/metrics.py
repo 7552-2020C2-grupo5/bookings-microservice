7d97741d050aa86a25f62aa8152e272769214428
CrossNox <ijmermet@gmail.com>
2021-02-27 20:26:07 -0300

    RFC: Issue22 (#23)
    * fix
    
    * patch bump

--- booking_microservice/controllers/metrics.py
+++ booking_microservice/controllers/metrics.py
@@ -1,31 +1,62 @@
 """Metrics controller."""
+import logging
+from datetime import date
 from datetime import timedelta as td
+from functools import wraps
 
 from sqlalchemy import func
 
 from booking_microservice.constants import BlockChainStatus, BookingStatus
 from booking_microservice.models import Booking
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-def prepare(name, query, cols):
-    return {"name": name, "data": [dict(zip(cols, r)) for r in query]}
+
+def isodate(x):
+    try:
+        return date.fromisoformat(x)
+    except TypeError:
+        return x
 
 
-def pad(metric, start_date, end_date):
-    dates = [d.get("date") for d in metric.get("data")]
+def pad(count, start_date: date, end_date: date):
+    dates = [x.get("date") for x in count]
 
     current_date = start_date
 
     while current_date <= end_date:
-        if current_date.isoformat() not in dates:
-            metric["data"].append({"date": current_date.isoformat(), "value": 0})
+        if current_date not in dates:
+            count.append({"date": current_date, "value": 0.0})
         current_date += td(days=1)
 
-    metric["data"] = sorted(metric["data"], key=lambda x: x.get("date"))
+    count = sorted(count, key=lambda x: x.get("date"))
 
-    return metric
+    return count
 
 
+def data_map(count):
+    return [{"date": isodate(rdate), "value": rval} for rdate, rval in count]
+
+
+def prepare_metric(name):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            count = f(*args, **kwargs)
+            count = data_map(count)
+            count = pad(count, *args)
+            return {
+                "name": name,
+                "data": count,
+            }
+
+        return wrapper
+
+    return decorator
+
+
+@prepare_metric("revenue_per_day")
 def revenue_per_day(start_date, end_date):
     count = (
         Booking.query.filter(
@@ -33,18 +64,12 @@ def revenue_per_day(start_date, end_date):
             & (Booking.booking_status == BookingStatus.ACCEPTED.value)
             & (Booking.blockchain_status == BlockChainStatus.CONFIRMED.value)
         )
-        .with_entities(
-            func.date(Booking.booking_date),
-            func.sum(Booking.total_price),
-            func.count(Booking.id),
-        )
+        .with_entities(func.date(Booking.booking_date), func.sum(Booking.total_price),)
         .group_by(func.date(Booking.booking_date))
         .order_by(func.date(Booking.booking_date))
         .all()
     )
-    metric = prepare("revenue_per_day", count, ["date", "value"])
-    metric = pad(metric, start_date, end_date)
-    return metric
+    return count
 
 
 all_metrics = [revenue_per_day]
