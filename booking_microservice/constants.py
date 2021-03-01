3f22a37fc772c43aa206133da00f0f21b9fce80d
CrossNox <ijmermet@gmail.com>
2021-03-01 01:28:06 -0300

    add

--- booking_microservice/constants.py
+++ booking_microservice/constants.py
@@ -19,3 +19,5 @@ class BookingStatus(Enum):
 DEFAULT_VERIFICATION_URL = (
     "https://tokens-microservice.herokuapp.com/v1/tokens/verification"
 )
+BOOKBNB_TOKEN = "bookbnb_token"
+
--- booking_microservice/exceptions.py
+++ booking_microservice/exceptions.py
@@ -3,3 +3,14 @@
 
 class BookingDoesNotExist(Exception):
     pass
+
+class ServerTokenError(Exception):
+    pass
+
+
+class UnsetServerToken(Exception):
+    pass
+
+
+class InvalidEnvironment(Exception):
+    pass
